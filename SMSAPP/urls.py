0aec273336977f073cedf9f5044bad6ad82109cf
City Coder <eit_solution@yahoo.com>
2022-10-10 14:43:21 +0100

    initial commit

---
+++ SMSAPP/urls.py
@@ -0,0 +1,15 @@
+import imp
+from unicodedata import name
+from django import views
+from django.urls import path
+from . import views
+
+
+urlpatterns = [ 
+    path('', views.index, name='index'),
+    path('<int:id>', views.view_student, name='view_student'),
+    path('add/', views.add, name='add'),
+    path('edit/<int:id>', views.edit, name='edit'),
+    path('delete/<int:id>', views.delete, name='delete')
+    
+]
